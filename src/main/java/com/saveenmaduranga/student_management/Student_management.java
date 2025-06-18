de7977e7b5ab3fe37a40f53fce30c519a5300729
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-06-18 13:31:57 +0530

    initial commit

---
+++ src/main/java/com/saveenmaduranga/student_management/Student_management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.saveenmaduranga.student_management;
+
+/**
+ *
+ * @author Saveen Maduranga
+ */
+public class Student_management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
